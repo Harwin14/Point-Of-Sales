49081638a9300e6811c4ccd0a3bcd34300d7c622
Agung Harwin N <agungharwinn@gmail.com>
2022-11-23 19:10:44 +0700

    purchases done , sequence cant reset daily

--- routes/goods.js
+++ routes/goods.js
@@ -66,69 +66,69 @@ module.exports = (db) => {
             res.send(e);
         }
     });
-    router.post('/add', isLoggedIn, (req, res) => {
-        let sampleFile;
-        let uploadPath;
+    // router.post('/add', isLoggedIn, (req, res) => {
+    //     let sampleFile;
+    //     let uploadPath;
 
-        if (!req.files || Object.keys(req.files).length === 0) {
-            return res.status(400).send('No files were uploaded.');
-        }
+    //     if (!req.files || Object.keys(req.files).length === 0) {
+    //         return res.status(400).send('No files were uploaded.');
+    //     }
 
-        // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
-        sampleFile = req.files.sampleFile;
-        const imagesfiles = `${Date.now()}-${sampleFile.name}`
-        uploadPath = path.join(__dirname, '..', 'public', 'images', 'upload', imagesfiles);
+    //     // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
+    //     sampleFile = req.files.sampleFile;
+    //     const imagesfiles = `${Date.now()}-${sampleFile.name}`
+    //     uploadPath = path.join(__dirname, '..', 'public', 'images', 'upload', imagesfiles);
 
-        // Use the mv() method to place the file somewhere on your server
-        sampleFile.mv(uploadPath, function (err) {
-            if (err)
-                return res.status(500).send(err);
-
-            const { barcode, name, stock, purchaseprice, sellingprice, unit } = req.body
-            console.log(barcode, name, stock, purchaseprice, sellingprice)
-
-            db.query('INSERT INTO goods (barcode, name, stock, purchaseprice, sellingprice, unit, picture) VALUES ($1, $2, $3, $4, $5, $6, $7)', [barcode, name, stock, purchaseprice, sellingprice, unit, imagesfiles])
-            if (err) { console.log(err)
-                return console.error(err.message);
-            }
-            res.redirect('/goods')
-        })
-
-    });
-  
-
-    // router.post('/add', isLoggedIn, async (req, res, next) => {
-    //     try {
-    //         let sampleFile;
-    //         let uploadPath;
-
-    //         if (!req.files || Object.keys(req.files).length === 0) {
-    //             return res.status(400).send('No files were uploaded.');
-    //         }
-
-    //         // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
-    //         sampleFile = req.files.sampleFile;
-    //         const imagesfiles = `${Date.now()}-${sampleFile.name}`
-    //         uploadPath = path.join(__dirname, '..', 'public', 'images', 'upload', imagesfiles);
-
-    //         // Use the mv() method to place the file somewhere on your server
-    //         sampleFile.mv(uploadPath)
+    //     // Use the mv() method to place the file somewhere on your server
+    //     sampleFile.mv(uploadPath, function (err) {
+    //         if (err)
+    //             return res.status(500).send(err);
 
     //         const { barcode, name, stock, purchaseprice, sellingprice, unit } = req.body
-    //         const { rows: goods } = await db.query('SELECT * FROM goods" WHERE barcode = $1', [barcode])
-    //         if (goods.length > 0) {
-    //             req.flash('error', 'Product already exist!')
-    //             return res.redirect('/add')
+    //         console.log(barcode, name, stock, purchaseprice, sellingprice)
+
+    //         db.query('INSERT INTO goods (barcode, name, stock, purchaseprice, sellingprice, unit, picture) VALUES ($1, $2, $3, $4, $5, $6, $7)', [barcode, name, stock, purchaseprice, sellingprice, unit, imagesfiles])
+    //         if (err) { console.log(err)
+    //             return console.error(err.message);
     //         }
-
-    //         await db.query('INSERT INTO goods (barcode, name, stock, purchaseprice, sellingprice, unit, picture) VALUES ($1, $2, $3, $4, $5, $6, $7)', [barcode, name, stock, purchaseprice, sellingprice, unit, imagesfiles])
-
     //         res.redirect('/goods')
-    //     } catch (error) {
-    //         console.log(error)
-    //         res.send(error)
-    //     }
-    // })
+    //     })
+
+    // });
+  
+
+    router.post('/add', isLoggedIn, async (req, res, next) => {
+        try {
+            let sampleFile;
+            let uploadPath;
+
+            if (!req.files || Object.keys(req.files).length === 0) {
+                return res.status(400).send('No files were uploaded.');
+            }
+
+            // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
+            sampleFile = req.files.sampleFile;
+            const imagesfiles = `${Date.now()}-${sampleFile.name}`
+            uploadPath = path.join(__dirname, '..', 'public', 'images', 'upload', imagesfiles);
+
+            // Use the mv() method to place the file somewhere on your server
+            sampleFile.mv(uploadPath)
+
+            const { barcode, name, stock, purchaseprice, sellingprice, unit } = req.body
+            const { rows: goods } = await db.query('SELECT * FROM goods WHERE barcode = $1', [barcode])
+            if (goods.length > 0) {
+                req.flash('error', 'Product already exist!')
+                return res.redirect('/add')
+            }
+
+            await db.query('INSERT INTO goods (barcode, name, stock, purchaseprice, sellingprice, unit, picture) VALUES ($1, $2, $3, $4, $5, $6, $7)', [barcode, name, stock, purchaseprice, sellingprice, unit, imagesfiles])
+
+            res.redirect('/goods')
+        } catch (error) {
+            console.log(error)
+            res.send(error)
+        }
+    })
     router.get('/edit/:barcode', isLoggedIn, async (req, res, next) => {
         try {
             const { barcode } = req.params
@@ -148,15 +148,13 @@ module.exports = (db) => {
 
     router.post('/edit/:barcode', isLoggedIn, async (req, res, next) => {
         try {
-            const { barcode } = req.params
-            const { name, stock, purchase, selling, unit } = req.body
-
             let sampleFile;
             let uploadPath;
-
+            const { barcode } = req.params
+            const { name, stock, purchaseprice, sellingprice, unit } = req.body
             if (!req.files || Object.keys(req.files).length === 0) {
                 await db.query('UPDATE goods SET name=$1, stock=$2, purchaseprice=$3, sellingprice=$4, unit=$5 WHERE barcode=$6',
-                    [name, stock, purchase, selling, unit, barcode])
+                [name, stock, purchaseprice, sellingprice, unit, barcode])
             } else {
                 // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
                 sampleFile = req.files.sampleFile;
@@ -164,10 +162,14 @@ module.exports = (db) => {
                 uploadPath = path.join(__dirname, '..', 'public', 'images', 'upload', imagesfiles);
                 sampleFile.mv(uploadPath)
 
-                await db.query('UPDATE goods SET name=$1, stock=$2, purchaseprice=$3, sellingprice=$4, unit=$5, picture=$6 WHERE barcode=$7', [name, stock, purchase, selling, unit, imagesfiles, barcode])
+                const { barcode } = req.params
+                const { name, stock, purchaseprice, sellingprice, unit } = req.body
+                await db.query('UPDATE goods SET name=$1, stock=$2, purchaseprice=$3, sellingprice=$4, unit=$5, picture=$6 WHERE barcode=$7', 
+                [name, stock, purchaseprice, sellingprice, unit, imagesfiles, barcode])
             }
             res.redirect('/goods')
         } catch (err) {
+            console.log(err)
             res.send(err)
         }
     })
--- routes/purchases.js
+++ routes/purchases.js
@@ -33,9 +33,12 @@ module.exports = (db) => {
     const sortMode = req.query.order[0].dir
 
     const total = await db.query(`select count(*) as total from purchases${params.length > 0 ? ` where ${params.join(' or ')}` : ''}`)
-//    const data = await db.query(`select * from purchases${params.length > 0 ? ` where ${params.join(' or ')}` : ''} order by ${sortBy} ${sortMode} limit ${limit} offset ${offset} `)
+   // const users  = await db.query('SELECT * FROM users ORDER BY userid')
+
+    //const data = await db.query(`select * from purchases${params.length > 0 ? ` where ${params.join(' or ')}` : ''} order by ${sortBy} ${sortMode} limit ${limit} offset ${offset} `)
     const data = await db.query(`SELECT p.*, s.* FROM purchases as p LEFT JOIN suppliers as s ON p.supplier = s.supplierid${params.length > 0 ? ` where ${params.join(' or ')}` : ''} order by ${sortBy} ${sortMode} limit ${limit} offset ${offset} `)
     const response = {
+     // "users": users.rows,
       "draw": Number(req.query.draw),
       "recordsTotal": total.rows[0].total,
       "recordsFiltered": total.rows[0].total,
@@ -43,127 +46,121 @@ module.exports = (db) => {
     }
     res.json(response)
   })
- 
+
 
   router.get('/create', isLoggedIn, async (req, res, next) => {
     try {
-      const  { rows }  = await db.query('INSERT INTO purchases(totalsum) VALUES(0) returning *')
+      const { userid } = req.session.user
+      const { rows } = await db.query('INSERT INTO purchases(totalsum, operator) VALUES(0, $1) returning *', [userid])
       res.redirect(`/purchases/show/${rows[0].invoice}`)
     } catch (e) {
       console.log(e)
       res.send(e);
     }
   });
+
   router.get('/show/:invoice', isLoggedIn, async (req, res, next) => {
     try {
-      const  purchases  = await db.query('SELECT * FROM purchases WHERE invoice = $1', [req.params.invoice])
+      const { invoice } = req.params
+      const purchases = await db.query('SELECT p.*, s.* FROM purchases as p LEFT JOIN suppliers as s ON p.supplier = s.supplierid where invoice = $1', [invoice])
+      const  users  = await db.query('SELECT * FROM users ORDER BY userid')
       const { rows: goods } = await db.query('SELECT barcode, name FROM goods ORDER BY barcode')
       const { rows } = await db.query('SELECT * FROM suppliers ORDER BY supplierid')
-      console.log(rows)
-     // const invoice = req.body.invoice || rows.length > 0 ? rows[0].invoice : ''
-     // const data = await db.query('SELECT g.barcode, g.name, pi.quantity, pi.purchaseprice, pi.totalprice, g.barcode, g.name FROM purchaseitems as pi LEFT JOIN goods as g ON pi.itemcode = g.barcode WHERE pi.invoice = $1 ORDER BY pi.purchaseprice ',[invoice])
+     
+      // const invoice = req.body.invoice || rows.length > 0 ? rows[0].invoice : ''
+      // const data = await db.query('SELECT g.barcode, g.name, pi.quantity, pi.purchaseprice, pi.totalprice, g.barcode, g.name FROM purchaseitems as pi LEFT JOIN goods as g ON pi.itemcode = g.barcode WHERE pi.invoice = $1 ORDER BY pi.purchaseprice ',[invoice])
       res.render('purchases/form', {
         currentPage: 'Purchases',
         user: req.session.user,
         purchases: purchases.rows[0],
         goods,
+        users,
         supplier: rows,
         moment,
         //data: data.rows
       })
     } catch (e) {
       res.send(e);
-    } 
-  }); 
+    }
+  });
 
   router.post('/show/:invoice', isLoggedIn, async (req, res) => {
     try {
       const { invoice } = req.params
-      const {  totalsum, supplier, operator } = req.body
-    const a = await db.query('UPDATE purchases SET  totalsum = $1, supplier = $2, operator = $3  WHERE invoice = $4',[ totalsum, supplier, operator, invoice])
-   
-      console.log(a)
+      const { totalsum, supplier } = req.body
+      await db.query('UPDATE purchases SET totalsum = $1, supplier = $2 WHERE invoice = $3', [totalsum, supplier, invoice])
+
       req.flash('success', 'Transaction Success!')
       res.redirect('/purchases')
     } catch (error) {
       console.log(error)
       req.flash('error', 'Transaction Fail!')
       return res.redirect('/purchases')
-    } 
-  }) 
+    }
+  })
 
 
   router.get('/goods/:barcode', isLoggedIn, async (req, res) => {
     try {
-     const { barcode } = req.params
-     const { rows } = await db.query('SELECT * FROM goods WHERE barcode = $1', [barcode]);
-     //console.log(rows, barcode )
-     res.json(rows[0])
+      const { barcode } = req.params
+      const { rows } = await db.query('SELECT * FROM goods WHERE barcode = $1', [barcode]);
+     
+      res.json(rows[0])
     } catch (err) {
-     res.send(err)
-    }  
+      res.send(err)
+    }
   })
 
   router.post('/additem', isLoggedIn, async (req, res) => {
     try {
-     const { invoice, itemcode, quantity } = req.body
-     await db.query('INSERT INTO purchaseitems (invoice, itemcode, quantity)VALUES ($1, $2, $3) returning *', [invoice, itemcode, quantity]);
-     const { rows } = await db.query('SELECT * FROM purchases WHERE invoice = $1', [invoice])
-    // console.log(detail)
-     res.json(rows[0])
+      const { invoice, itemcode, quantity } = req.body
+      await db.query('INSERT INTO purchaseitems (invoice, itemcode, quantity)VALUES ($1, $2, $3) returning *', [invoice, itemcode, quantity]);
+      const { rows } = await db.query('SELECT * FROM purchases WHERE invoice = $1', [invoice])
+    
+      res.json(rows[0])
     } catch (err) {
       console.log(err)
       res.send(err)
     }
   })
 
-  // router.get('/details/:invoice', isLoggedIn, async (req, res) => {
-  //   try {
-  //    const { invoice } = req.params
-  //    const  data  = await db.query('SELECT purchaseitems.*, Goods.name FROM purchaseitems  LEFT JOIN Goods ON purchaseitems.itemcode = Goods.barcode WHERE purchaseitems.invoice = $1 ORDER BY purchaseitems.id', [invoice]);
-  //    const response = {
-  //     "data": data.rows }
-  //    console.log(response)
-  //    res.json(response)
-  //   } catch (err) {
-  //     console.log(err)
-  //    res.send(err)
-  //   }
-  // })
   router.get('/details/:invoice', isLoggedIn, async (req, res, next) => {
     try {
-        const { invoice } = req.params
-        const { rows: data } = await db.query('SELECT purchaseitems.*, goods.name FROM purchaseitems LEFT JOIN goods ON purchaseitems.itemcode = goods.barcode WHERE purchaseitems.invoice = $1 ORDER BY purchaseitems.id', [invoice])
+      const { invoice } = req.params
+      const { rows: data } = await db.query('SELECT purchaseitems.*, goods.name FROM purchaseitems LEFT JOIN goods ON purchaseitems.itemcode = goods.barcode WHERE purchaseitems.invoice = $1 ORDER BY purchaseitems.id', [invoice])
 
-        res.json(data)
+      res.json(data)
     } catch (err) {
-        console.log(err)
+      console.log(err)
     }
-});
+  });
+
+ 
+  router.get('/deleteitems/:id', isLoggedIn, async (req, res, next) => {
+    try {
+      const { id } = req.params
+      const { rows: data } = await db.query('DELETE FROM purchaseitems WHERE id = $1 returning *', [id])
+      
+      req.flash('success', 'Transaction deleted successfully') 
+      res.redirect(`/purchases/show/${data[0].invoice}`)
+    } catch (err) {
+      req.flash('error', 'Please, Edit and Delete items first ')
+
+      console.log(err)
+    }
+  });
+
   router.get('/delete/:invoice', isLoggedIn, async (req, res, next) => {
     try {
       const { invoice } = req.params
-     const s = await db.query('DELETE FROM purchaseitems WHERE invoice = $1', [invoice])
-     //const s = await db.query('DELETE FROM purchases as p LEFT OUTER JOIN suppliers as s ON p.supplier = s.supplierid WHERE  p.invoice = $1', [invoice])
-
-      console.log(invoice, s)
+      await db.query('DELETE FROM purchases WHERE invoice = $1', [invoice])
+    
       req.flash('success', 'Transaction deleted successfully')
       res.redirect('/purchases');
     } catch (err) {
-      req.flash('error', err)
+      req.flash('error', 'Please, Edit and Delete items first ')
       return res.redirect('/purchases')
     }
-});
-router.get('/deleteitems/:id', isLoggedIn, async (req, res, next) => {
-  try {
-      const { id } = req.params
-      const { rows: data } = await db.query('DELETE FROM purchaseitems WHERE id = $1 returning *', [id])
-
-      console.log(data)
-      res.redirect(`/purchases/show/${data[0].invoice}`)
-  } catch (err) {
-      console.log(err)
-  }
-});
+  });
   return router;
 }     
